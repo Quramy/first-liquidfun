d7b82d79a19208c5a25e5a84eaff7f6b7f7e62cb
y-kurami <yosuke.kurami@gmail.com>
2014-09-02 03:38:07 +0900

    add dynamic bodies

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,9 +3,13 @@
 //  注! グローバル変数にworldという名前でb2World用変数を用意しておかないと落ちる.
 var world;
 var boundsNodes = [[-2, 0], [2, 0], [2, 4], [-2, 4]]; //  境界形状
+var floaters = [
+	{nodes:[[-0.1, -0.2],[0.1, -0.2],[0.1, 0.2],[-0.1, 0.2]], pos:[0.5, 2]},
+	{nodes:[[0, 0.2],[0.1732, -0.0866],[-0.1732, -0.0866]], pos:[-1.5, 3]}
+];
 var pgDefs = [		//  particleGroup毎の初期形状
-	{nodes:[[0.5, 0.1], [1.9, 0.1], [1.9, 2.5], [0.5, 1.0]], color:[255, 0, 0 , 255]},
-	{nodes:[[-0.5, 0.1], [-1.9, 0.1], [-1.9, 2.5], [-0.5, 1.0]], color:[0, 0, 255 , 255]}
+	{nodes:[[0.5, 0.1], [1.9, 0.1], [1.9, 2.5], [0.5, 1.0]]},
+	{nodes:[[-0.5, 0.1], [-1.9, 0.1], [-1.9, 2.5], [-0.5, 1.0]]}
 ];
 var timeStep = 1.0 / 60.0, velocityIterations = 8, positionIterations = 3;
 
@@ -13,20 +17,36 @@ var damBreak = {
 
 	init: function() {
 		var gravity = new b2Vec2(0, -10);
-		var bodyDef = new b2BodyDef();
-		var ground;
-
-		var chainShape = new b2ChainShape();
+		var boundsBody, boxShape;
 		var psd, particleSystem;
 
+		// 環境定義
 		world = new b2World(gravity);
-		ground = world.CreateBody(bodyDef);
 
-		chainShape.vertices = boundsNodes.map(function(node){
+		// 剛体(static) 関連 
+		boundsBody= world.CreateBody(new b2BodyDef());
+		boxShape = new b2ChainShape();
+		boxShape.vertices = boundsNodes.map(function(node){
 			return new b2Vec2(node[0], node[1]);
 		});
-		chainShape.CreateLoop();
-		ground.CreateFixtureFromShape(chainShape, 0);
+		boxShape.CreateLoop();
+		boundsBody.CreateFixtureFromShape(boxShape, 0);
+
+		// 剛体(dyanmic)関連ここから 
+		floaters.forEach(function(floaterDef){
+			var dynamicBodyDef = new b2BodyDef(), body, shape;
+			dynamicBodyDef.type = b2_dynamicBody;
+			body = world.CreateBody(dynamicBodyDef);
+			shape = new b2ChainShape();
+			shape.vertices = floaterDef.nodes.map(function(node){
+				return new b2Vec2(node[0], node[1]);
+			});
+			shape.CreateLoop();
+			body.CreateFixtureFromShape(shape, 1);
+			body.SetTransform(new b2Vec2(floaterDef.pos[0], floaterDef.pos[1]), 0);
+			// 質量定義
+			body.SetMassData(new b2MassData(0.1, new b2Vec2(0, 0), 0.03));
+		});
 
 		// Particle モジュール関連ここから
 		psd = new b2ParticleSystemDef();
@@ -40,13 +60,9 @@ var damBreak = {
 			shape.vertices = def.nodes.map(function(node){
 				return new b2Vec2(node[0], node[1]);
 			});
-			//pd.flags = b2_tensileParticle | b2_colorMixingParticle;
-			pd.flags = b2_colorMixingParticle;
-			pd.color.Set.apply(pd.color, def.color);
 			pd.shape = shape;
 			particleSystem.CreateParticleGroup(pd);
 		});
-		// Particle モジュール関連ここまで.
 
 	},
 	update: function(){
@@ -70,18 +86,33 @@ var render = function(){
 var d3Renderer = {
 	init: function(){
 		var viz = d3.select('body').append('svg').attr('id', 'viz').append('g').classed('world', true);
-		var bounds = d3.svg.line().x(function(node){return node[0];}).y(function(node){return node[1];});
-		viz.append('path').attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', 0.01)
-		.attr('d', bounds(boundsNodes) + ' Z');
-
 		d3Renderer.resize();
 	},
 	render: function(world){
 		var viz = d3.select('svg#viz g.world');
-
-		var system = world.particleSystems[0];
-		var particleGroup = viz.selectAll('g.particle').data(system.particleGroups)
-		var positionBuf = system.GetPositionBuffer(), colorBuf = system.GetColorBuffer();
+		d3Renderer.drawBodies(viz, world.bodies);
+		d3Renderer.drawParicles(viz, world.particleSystems[0]);
+	},
+	drawBodies: function(selection, bodies){ // 剛体描画用 
+		var bounds = d3.svg.line().x(function(vec){return vec.x;}).y(function(vec){return vec.y;});
+		var bodyGroups = selection.selectAll('g.body').data(bodies, function(b){
+			return b.ptr;
+		});
+		bodyGroups.enter().append('g').classed('body', true).attr('fill', 'none').attr('stroke', 'black').attr('stroke-width', 0.01);
+		bodyGroups.each(function(b){
+			d3.select(this).selectAll('path').data(b.fixtures).enter().append('path').attr('d', function(fixture){
+				return bounds(fixture.shape.vertices);
+			});
+		});
+		bodyGroups.attr('transform', function(b){
+			var pos = b.GetPosition(), angle = b.GetAngle() * 180 / Math.PI;
+			return 'translate(' + pos.x + ', ' + pos.y + '), rotate(' + angle + ')';
+		});
+		bodyGroups.exit().remove();
+	},
+	drawParicles: function(selection, system){ // 流体粒子描画用 
+		var particleGroup = selection.selectAll('g.particle').data(system.particleGroups)
+		var positionBuf = system.GetPositionBuffer();
 		particleGroup.enter().append('g').classed('particle', true).attr('fill', function(d, i){
 			return d3.hsl((i * 77 + 200) % 360, 0.8, 0.8);
 		});
@@ -93,13 +124,7 @@ var d3Renderer = {
 				return positionBuf[(i + offset) * 2];
 			}).attr('cy', function(d, i){
 				return positionBuf[(i + offset) * 2 + 1];
-			});/*.attr('fill', function(d, i){
-				return d3.rgb(
-					colorBuf[(i + offset) * 4],
-					colorBuf[(i + offset) * 4 + 1],
-					colorBuf[(i + offset) * 4 + 2]
-				);
-			});*/
+			});
 			dataSet.exit().remove();
 		});
 		particleGroup.exit().remove();
